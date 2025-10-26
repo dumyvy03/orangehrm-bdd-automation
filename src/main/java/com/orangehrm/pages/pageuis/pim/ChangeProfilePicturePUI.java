4a739c5da5067db7da87763a8d837ae42a7cd9b6
Du My Vy <dumyvy03@gmail.com>
2025-10-26 21:44:05 +0700

    Add ChangeProfilePicturePUI

---
+++ src/main/java/com/orangehrm/pages/pageuis/pim/ChangeProfilePicturePUI.java
@@ -0,0 +1,5 @@
+package com.orangehrm.pages.pageuis.pim;
+
+public class ChangeProfilePicturePUI {
+    public static final String SAVE_PROFILE_PICTURE_BUTTON = "Xpath=//h6[text()='Change Profile Picture']/following-sibling::form//button[normalize-space(.)='Save']";
+}
