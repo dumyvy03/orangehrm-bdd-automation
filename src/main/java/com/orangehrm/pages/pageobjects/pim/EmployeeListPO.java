e259a9dde50d28d0b4c37ee6606753b80fa6e6e9
Du My Vy <dumyvy03@gmail.com>
2025-10-29 19:56:44 +0700

    Add employee search and edit actions, and refactor base structure in EmployeeListPO

--- src/main/java/com/orangehrm/pages/pageobjects/pim/EmployeeListPO.java
+++ src/main/java/com/orangehrm/pages/pageobjects/pim/EmployeeListPO.java
@@ -1,15 +1,14 @@
 package com.orangehrm.pages.pageobjects.pim;
 
+import com.orangehrm.commons.BasePage;
 import com.orangehrm.commons.PageGenerator;
-import com.orangehrm.pages.pageobjects.commons.SidebarPO;
 import com.orangehrm.pages.pageuis.pim.EmployeeListPUI;
 import org.openqa.selenium.WebDriver;
 
-public class EmployeeListPO extends SidebarPO {
+public class EmployeeListPO extends BasePage {
     private WebDriver driver;
 
     public EmployeeListPO(WebDriver driver) {
-        super(driver);
         this.driver = driver;
     }
 
@@ -21,7 +20,7 @@ public class EmployeeListPO extends SidebarPO {
     }
 
     public void clickDeleteEmployeeButton(String employeeID) {
-        scrollIntoView(driver, EmployeeListPUI.DYNAMIC_DELETE_BUTTON_BY_ID, employeeID);
+        scrollPageToBottomByJS(driver);
         sleep(1);
         clickElement(driver, EmployeeListPUI.DYNAMIC_DELETE_BUTTON_BY_ID, employeeID);
     }
@@ -34,5 +33,28 @@ public class EmployeeListPO extends SidebarPO {
     public boolean isEmployeeDeleted(String employeeID) {
         waitForLoadingIconInvisible(driver);
         return isElementUnDisplayed(driver, EmployeeListPUI.DYNAMIC_EMPLOYEE_ID, employeeID);
+    }
+
+    public void enterEmployeeNameTextbox(String employeeName) {
+        sleep(3);
+        sendKeysElement(driver, EmployeeListPUI.EMPLOYEE_NAME_TEXTBOX, employeeName);
+    }
+
+    public void selectSubUnitDropdown(String subUnit) {
+        sleep(2);
+        selectDropdownCustomer(driver, EmployeeListPUI.SUB_UNIT_DROPDOWN_PARENT, EmployeeListPUI.SUB_UNIT_DROPDOWN_CHILD, subUnit);
+    }
+
+    public void clickSearchButton() {
+        sleep(1);
+        clickElement(driver, EmployeeListPUI.SEARCH_BUTTON);
+    }
+
+    public PersonalDetailsPO clickEditButtonEmployeeList(String employeeId) {
+        scrollPageToBottomByJS(driver);
+        sleep(1);
+        clickElement(driver, EmployeeListPUI.DYNAMIC_EDIT_BUTTON_BY_ID, employeeId);
+        waitForLoadingIconInvisible(driver);
+        return PageGenerator.getPersonalDetailsPage(driver);
     }
 }
