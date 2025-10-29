3c794a997e6a270a992a92c6260784ed2e161f1d
Du My Vy <dumyvy03@gmail.com>
2025-10-29 20:11:23 +0700

    Add username and password error message locators in LoginPUI

--- src/main/java/com/orangehrm/pages/pageuis/LoginPUI.java
+++ src/main/java/com/orangehrm/pages/pageuis/LoginPUI.java
@@ -5,5 +5,7 @@ public class LoginPUI {
     public static final String PASSWORD_TEXTBOX = "Name=password";
     public static final String LOGIN_BUTTON = "Css=button.orangehrm-login-button";
     public static final String LOGIN_ERROR_TEXT = "Xpath=//div[@class='orangehrm-login-error']//p";
+    public static final String USERNAME_ERROR_MESSAGE = "Xpath=//label[text()='Username']/parent::div//following-sibling::span[text()='Required']";
+    public static final String PASSWORD_ERROR_MESSAGE = "Xpath=//label[text()='Password']/parent::div//following-sibling::span[text()='Required']";
 }
 
