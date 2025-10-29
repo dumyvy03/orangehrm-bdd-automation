b376c3522c2dbd8730910922a32de86bb52ba1d3
Du My Vy <dumyvy03@gmail.com>
2025-10-29 20:20:32 +0700

    Update name field getters and add enterFirstNameTextbox in PersonalDetailsPO

--- src/main/java/com/orangehrm/pages/pageobjects/pim/PersonalDetailsPO.java
+++ src/main/java/com/orangehrm/pages/pageobjects/pim/PersonalDetailsPO.java
@@ -1,12 +1,11 @@
 package com.orangehrm.pages.pageobjects.pim;
 
-import com.orangehrm.pages.pageobjects.commons.SidebarPO;
-import com.orangehrm.pages.pageuis.pim.AddEmployeePUI;
+import com.orangehrm.commons.PageGenerator;
 import com.orangehrm.pages.pageuis.pim.PersonalDetailsPUI;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 
-public class PersonalDetailsPO extends SidebarPO {
+public class PersonalDetailsPO extends EmployeeNavigationPO {
     private WebDriver driver;
 
     public PersonalDetailsPO(WebDriver driver) {
@@ -14,16 +13,11 @@ public class PersonalDetailsPO extends SidebarPO {
         this.driver = driver;
     }
 
-
-    public String getFirstNameTextbox() {
+    public String getFirstName() {
         return getAttributeValue(driver, PersonalDetailsPUI.FIRSTNAME_TEXTBOX, "value");
     }
 
-    public String getMiddleNameTextbox() {
-        return getAttributeValue(driver, PersonalDetailsPUI.MIDDLE_TEXTBOX, "value");
-    }
-
-    public String getLastNameTextbox() {
+    public String getLastName() {
         return getAttributeValue(driver, PersonalDetailsPUI.LASTNAME_TEXTBOX, "value");
     }
 
@@ -33,7 +27,84 @@ public class PersonalDetailsPO extends SidebarPO {
     }
 
     public String getEmployeeIdAtDetailsPage() {
-        return getAttributeValue(driver, AddEmployeePUI.EMPLOYEE_ID_TEXTBOX, "value");
+        return getAttributeValue(driver, PersonalDetailsPUI.EMPLOYEE_ID_TEXTBOX, "value");
     }
 
+    public void enterFirstNameTextbox(String firstName) {
+        sendKeysElement(driver, PersonalDetailsPUI.FIRSTNAME_TEXTBOX, firstName);
+        sleep(1);
+    }
+
+    public void enterLastNameTextbox(String lastName) {
+        sendKeysElement(driver, PersonalDetailsPUI.LASTNAME_TEXTBOX, lastName);
+        sleep(1);
+    }
+
+    public void enterDriverLicenseTextbox(String driverLicense) {
+        sendKeysElement(driver, PersonalDetailsPUI.DRIVER_LICENSE_TEXTBOX, driverLicense);
+        sleep(1);
+    }
+
+    public void enterLicenseExpiryDateTextbox(String licenseExpiryDate) {
+        sendKeysElement(driver, PersonalDetailsPUI.LICENSE_EXPIRY_DATE_TEXTBOX, licenseExpiryDate);
+        sleep(1);
+    }
+
+    public void selectNationalityDropdown(String nationality) {
+        selectDropdownCustomer(driver, PersonalDetailsPUI.NATIONALITY_DROPDOWN_PARENT, PersonalDetailsPUI.NATIONALITY_DROPDOWN_CHILD, nationality);
+        sleep(1);
+    }
+
+    public void selectMaritalStatusDropdown(String maritalStatus) {
+        selectDropdownCustomer(driver, PersonalDetailsPUI.MARITAL_STATUS_DROPDOWN_PARENT, PersonalDetailsPUI.MARITAL_STATUS_DROPDOWN_CHILD, maritalStatus);
+        sleep(1);
+    }
+
+    public void enterDateOfBirthTextbox(String dateOfBirth) {
+        sendKeysElement(driver, PersonalDetailsPUI.DATE_OF_BIRTH_TEXTBOX, dateOfBirth);
+        sleep(1);
+    }
+
+    public void selectGenderRadioButton(String gender) {
+        checkElementByJS(driver, PersonalDetailsPUI.GENDER_RADIO_BUTTON, gender);
+        sleep(1);
+    }
+
+    public String getDriverLicenseNumberValue() {
+        return getAttributeValue(driver, PersonalDetailsPUI.DRIVER_LICENSE_TEXTBOX, "value");
+    }
+
+    public String getLicenseExpiryDateValue() {
+        return getAttributeValue(driver, PersonalDetailsPUI.LICENSE_EXPIRY_DATE_TEXTBOX, "value");
+    }
+
+    public String getSelectedNationalityText() {
+        return getTextElement(driver, PersonalDetailsPUI.NATIONALITY_SELECTED_TEXT);
+    }
+
+    public String getSelectedMaritalStatusText() {
+        return getTextElement(driver, PersonalDetailsPUI.MARITAL_STATUS_SELECTED_TEXT);
+    }
+
+    public String getDateOfBirthValue() {
+        return getAttributeValue(driver, PersonalDetailsPUI.DATE_OF_BIRTH_TEXTBOX, "value");
+    }
+
+    public boolean isGenderSelected(String gender) {
+        return isElementSelected(driver, PersonalDetailsPUI.GENDER_RADIO_BUTTON, gender);
+    }
+
+    public void clickSaveButtonPersonalDetails() {
+        clickElement(driver, PersonalDetailsPUI.SAVE_PERSONAL_DETAILS_BUTTON);
+        waitForLoadingIconInvisible(driver);
+    }
+
+    public ChangeProfilePicturePO openChangeProfilePicturePage() {
+        clickElement(driver, PersonalDetailsPUI.EMPLOYEE_AVATAR);
+        return PageGenerator.getChangeProfilePicturePage(driver);
+    }
+
+    public Dimension getAvatarSize() {
+        return getElementSize(driver, PersonalDetailsPUI.EMPLOYEE_AVATAR);
+    }
 }
