cadb11976c4fb4ed2680a6c5bc7d0138a1dde56c
Du My Vy <dumyvy03@gmail.com>
2025-11-02 19:35:20 +0700

    Add ValidationPUI class

---
+++ src/main/java/com/orangehrm/pages/pageuis/commons/ValidationPUI.java
@@ -0,0 +1,5 @@
+package com.orangehrm.pages.pageuis.commons;
+
+public class ValidationPUI {
+    public static final String FIRSTNAME_ERROR_TEXT = "Xpath=//input[@name='firstName']/parent::div/following-sibling::span";
+}
