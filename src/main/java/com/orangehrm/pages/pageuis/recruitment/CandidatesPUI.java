afbbb6a0107214db3bf1b90fef8b4695c3352812
Du My Vy <dumyvy03@gmail.com>
2025-11-08 23:09:33 +0700

    Add CandidatesPUI class

---
+++ src/main/java/com/orangehrm/pages/pageuis/recruitment/CandidatesPUI.java
@@ -0,0 +1,5 @@
+package com.orangehrm.pages.pageuis.recruitment;
+
+public class CandidatesPUI {
+    public static final String ADD_CANDIDATES_BUTTON = "Xpath=//button[normalize-space(.)='Add']";
+}
