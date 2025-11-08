d0c4073f8de69dbca360a601a3503259bb6adcb6
Du My Vy <dumyvy03@gmail.com>
2025-11-08 23:08:56 +0700

    Add CandidateProfilePO class

---
+++ src/main/java/com/orangehrm/pages/pageobjects/recruitment/CandidatesPO.java
@@ -0,0 +1,21 @@
+package com.orangehrm.pages.pageobjects.recruitment;
+
+import com.orangehrm.commons.BasePage;
+import com.orangehrm.commons.PageGenerator;
+import com.orangehrm.pages.pageuis.recruitment.CandidatesPUI;
+import org.openqa.selenium.WebDriver;
+
+public class CandidatesPO extends BasePage {
+    private WebDriver driver;
+
+    public CandidatesPO(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public AddCandidatePO clickAddButton() {
+        scrollToElement(driver, CandidatesPUI.ADD_CANDIDATES_BUTTON);
+        clickElement(driver, CandidatesPUI.ADD_CANDIDATES_BUTTON);
+        waitForLoadingIconInvisible(driver);
+        return PageGenerator.getAddCandidatePage(driver);
+    }
+}
