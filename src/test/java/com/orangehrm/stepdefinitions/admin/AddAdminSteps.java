f8447025d053ceca4c341ef48a680c95565e2396
Du My Vy <dumyvy03@gmail.com>
2025-11-08 22:26:48 +0700

    Add AddAdminSteps class

---
+++ src/test/java/com/orangehrm/stepdefinitions/admin/AddAdminSteps.java
@@ -0,0 +1,84 @@
+package com.orangehrm.stepdefinitions.admin;
+
+import com.orangehrm.commons.PageGenerator;
+import com.orangehrm.context.TestContext;
+import com.orangehrm.pages.pageobjects.admin.AddUserPO;
+import com.orangehrm.pages.pageobjects.admin.UserManagementPO;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.testng.Assert;
+
+import java.util.Map;
+
+public class AddAdminSteps {
+    private final TestContext testContext;
+    private UserManagementPO userManagementPage;
+    private String userName;
+    private AddUserPO addUserPage;
+
+    public AddAdminSteps(TestContext testContext) {
+        this.testContext = testContext;
+    }
+
+    @And("navigates to the Admin page")
+    public void navigatesToTheAdminPage() {
+        userManagementPage = PageGenerator.getSidebarPage(testContext.getDriver()).openAdminPage();
+    }
+
+    @And("clicks the Add button to open the Add User page")
+    public void theAdminClicksTheAddButtonToOpenTheAddUserForm() {
+        addUserPage = userManagementPage.clickAddButton();
+    }
+
+    @When("the admin enters and selects user information")
+    public void entersAndSelectsUserInformation(DataTable dataTable) {
+        Map<String, String> userInformationData = dataTable.asMap(String.class, String.class);
+        userName = userInformationData.get("Username");
+        addUserPage.enterUserInformation(userInformationData);
+    }
+
+    @And("clicks the Save button to add a new user")
+    public void clicksTheSaveButtonToAddANewUser() {
+        userManagementPage = addUserPage.clickSaveButton();
+    }
+
+    @Then("the new user is displayed in the user list")
+    public void theNewUserIsDisplayedInTheUserList() {
+        Assert.assertTrue(userManagementPage.isUserNameDisplayed(userName));
+    }
+
+    @When("the admin enters username {string}")
+    public void entersUsername(String userName) {
+        addUserPage.enterUserNameTextbox(userName);
+    }
+
+    @When("the admin enters password {string}")
+    public void entersPassword(String password) {
+        addUserPage.enterPasswordTextbox(password);
+    }
+
+    @When("the admin enters password {string} and confirm password {string}")
+    public void entersPasswordAndConfirmPassword(String password, String confirmPassword) {
+        addUserPage.enterPasswordTextbox(password);
+        addUserPage.enterConfirmPasswordTextbox(confirmPassword);
+    }
+
+    @Then("the confirm password field shows error {string}")
+    public void theConfirmPasswordFieldShowsError(String errorMessage) {
+        Assert.assertEquals(addUserPage.getConfirmPasswordErrorMessage(), errorMessage);
+    }
+
+
+    @When("the admin enters employee name {string} on Add User page")
+    public void theAdminEntersEmployeeNameOnAddUserPage(String employeeName) {
+        addUserPage.enterEmployeeNameTextbox(employeeName);
+    }
+
+
+    @Then("the suggestions dropdown displays {string}")
+    public void verifySuggestionsDropdownDisplays(String recordsMessage) {
+        Assert.assertEquals(addUserPage.getSuggestionDropdownMessage(), recordsMessage);
+    }
+}
