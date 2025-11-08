6772c9f3a29038c104daed0f1b4382066d6da559
Du My Vy <dumyvy03@gmail.com>
2025-11-08 23:08:35 +0700

    Add CandidateProfilePUI class

---
+++ src/main/java/com/orangehrm/pages/pageuis/recruitment/CandidateProfilePUI.java
@@ -0,0 +1,21 @@
+package com.orangehrm.pages.pageuis.recruitment;
+
+public class CandidateProfilePUI {
+    public static final String FIRSTNAME_TEXTBOX = "Css=input.orangehrm-firstname";
+
+    public static final String LASTNAME_TEXTBOX = "Css=input.orangehrm-lastname";
+
+    public static final String JOB_VACANCY_SELECTED_TEXT = "Xpath=//label[text()='Job Vacancy']/parent::div/following-sibling::div//div[@class='oxd-select-text-input']";
+
+    public static final String EMAIL_TEXTBOX = "Xpath=//label[text()='Email']/parent::div/following-sibling::div/input";
+
+    public static final String CONTACT_NUMBER_TEXTBOX = "Xpath=//label[text()='Contact Number']/parent::div/following-sibling::div/input";
+
+    public static final String RESUME_UPLOADED_FILE_NAME = "Xpath=//label[text()='Resume']/parent::div/following-sibling::div//p[contains(@class,'orangehrm-file-name')]";
+
+    public static final String KEYWORDS_TEXTBOX = "Xpath=//label[text()='Keywords']/parent::div/following-sibling::div//input";
+
+    public static final String DATE_OF_APPLICATION_TEXTBOX = "Xpath=//label[text()='Date of Application']/parent::div/following-sibling::div//input";
+
+    public static final String NOTES_TEXTAREA = "Xpath=//label[text()='Notes']/parent::div/following-sibling::div//textarea";
+}
