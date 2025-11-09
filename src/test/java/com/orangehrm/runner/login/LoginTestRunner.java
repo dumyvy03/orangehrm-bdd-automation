21a1da5187002ff57d518b1206f495f94b02396f
Du My Vy <dumyvy03@gmail.com>
2025-11-09 23:49:32 +0700

    Move TestRunner to login package and rename to LoginTestRunner

--- src/test/java/com/orangehrm/runner/login/LoginTestRunner.java
+++ src/test/java/com/orangehrm/runner/login/LoginTestRunner.java
@@ -1,10 +1,10 @@
-package com.orangehrm.runner;
+package com.orangehrm.runner.login;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = ("src/test/resources/features/pim/add_employee.feature"),
+        features = ("src/test/resources/features/login/Login.feature"),
         glue = {"com.orangehrm.stepdefinitions",
                 "com.orangehrm.hooks",
                 "com.orangehrm.context",
@@ -13,7 +13,7 @@ import io.cucumber.testng.CucumberOptions;
         plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
         monochrome = true
 )
-public class TestRunner extends AbstractTestNGCucumberTests {
+public class LoginTestRunner extends AbstractTestNGCucumberTests {
 }
 
 
