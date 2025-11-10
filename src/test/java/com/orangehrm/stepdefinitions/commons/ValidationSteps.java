3c2489da978d9cf1683cd7e41d70a4a8bc372f16
Du My Vy <dumyvy03@gmail.com>
2025-11-10 12:40:09 +0700

    Add steps for username and password error messages

--- src/test/java/com/orangehrm/stepdefinitions/commons/ValidationSteps.java
+++ src/test/java/com/orangehrm/stepdefinitions/commons/ValidationSteps.java
@@ -7,15 +7,24 @@ import io.cucumber.java.en.Then;
 import org.testng.Assert;
 
 public class ValidationSteps {
-    private final TestContext testContext;
+    private final ValidationPO validationPage;
 
     public ValidationSteps(TestContext testContext) {
-        this.testContext = testContext;
+        this.validationPage = PageGenerator.getValidationPage(testContext.getDriver());
     }
 
     @Then("the first name field shows error {string}")
     public void verifyFirstNameFieldShowsError(String errorMessage) {
-        ValidationPO validationPage = PageGenerator.getValidationPage(testContext.getDriver());
         Assert.assertEquals(validationPage.getFirstNameErrorMessage(), errorMessage);
+    }
+
+    @Then("the username field shows error {string}")
+    public void verifyUsernameErrorMessage(String errorMessage) {
+        Assert.assertEquals(validationPage.getUsernameErrorMessage(), errorMessage);
+    }
+
+    @Then("the password field shows error {string}")
+    public void verifyPasswordShowsError(String errorMessage) {
+        Assert.assertEquals(validationPage.getPasswordErrorMessage(), errorMessage);
     }
 }
