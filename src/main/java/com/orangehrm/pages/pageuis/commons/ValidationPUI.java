c7c7588c98a49d4dfbb99bca416819019b46d089
Du My Vy <dumyvy03@gmail.com>
2025-11-10 12:37:34 +0700

    Add constants for username and password error messages in ValidationPUI

--- src/main/java/com/orangehrm/pages/pageuis/commons/ValidationPUI.java
+++ src/main/java/com/orangehrm/pages/pageuis/commons/ValidationPUI.java
@@ -2,4 +2,8 @@ package com.orangehrm.pages.pageuis.commons;
 
 public class ValidationPUI {
     public static final String FIRSTNAME_ERROR_TEXT = "Xpath=//input[@name='firstName']/parent::div/following-sibling::span";
+
+    public static final String USERNAME_ERROR_MESSAGE = "Xpath=//label[text()='Username']/parent::div//following-sibling::span";
+
+    public static final String PASSWORD_ERROR_MESSAGE = "Xpath=//label[text()='Password']/parent::div//following-sibling::span";
 }
