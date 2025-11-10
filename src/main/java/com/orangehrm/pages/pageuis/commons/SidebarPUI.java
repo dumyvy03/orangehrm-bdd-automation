442cef878c2287529981cd669f48624aba4b5fab
Du My Vy <dumyvy03@gmail.com>
2025-11-10 12:10:22 +0700

    Add RECRUITMENT_LINK and ADMIN_LINK constants in SidebarPUI

--- src/main/java/com/orangehrm/pages/pageuis/commons/SidebarPUI.java
+++ src/main/java/com/orangehrm/pages/pageuis/commons/SidebarPUI.java
@@ -2,6 +2,9 @@ package com.orangehrm.pages.pageuis.commons;
 
 public class SidebarPUI {
     public static final String PIM_LINK = "Xpath=//ul[@class='oxd-main-menu']//span[text()='PIM']";
+
     public static final String RECRUITMENT_LINK = "Xpath=//ul[@class='oxd-main-menu']//span[text()='Recruitment']";
+
+    public static final String ADMIN_LINK = "Xpath=//ul[@class='oxd-main-menu']//span[text()='Admin']";
 
 }
