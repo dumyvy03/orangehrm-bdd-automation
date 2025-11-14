7947de315e6c095a80c54c20ddf9f703e8e409d9
Du My Vy <dumyvy03@gmail.com>
2025-11-14 12:38:08 +0700

    Refactor Hooks to use DriverFactory and simplify before and after logic

--- src/test/java/com/orangehrm/hooks/Hooks.java
+++ src/test/java/com/orangehrm/hooks/Hooks.java
@@ -1,14 +1,18 @@
 package com.orangehrm.hooks;
 
-import com.orangehrm.context.ScenarioContext;
-import com.orangehrm.context.TestContext;
-import com.orangehrm.helper.AllureHelper;
-import com.orangehrm.helper.EmployeeCleanupHelper;
-import io.cucumber.java.*;
+import com.orangehrm.commons.DriverFactory;
+import com.orangehrm.commons.PageGenerator;
+import com.orangehrm.pages.pageobjects.pim.EmployeeListPO;
+import com.orangehrm.stepdefinitions.pim.AddEmployeeSteps;
+import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import io.qameta.allure.Allure;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.xml.XmlTest;
 
@@ -17,22 +21,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Hooks {
-    private final TestContext testContext;
-
-    public Hooks(TestContext testContext) {
-        this.testContext = testContext;
-    }
-
-    @BeforeAll
-    public static void cleanAllureResults() {
-        AllureHelper.cleanResults();
-        AllureHelper.bringHistory();
-    }
-
-    @AfterAll
-    public static void createEnvFile() {
-        AllureHelper.createEnvironmentFile();
-    }
+    private WebDriver driver;
 
     @Before
     public void before() {
@@ -41,12 +30,12 @@ public class Hooks {
         if (browserName == null) {
             browserName = "chrome";
         }
-        testContext.init(browserName);
+        DriverFactory.createDriver(browserName);
     }
 
     @AfterStep
     public void takeScreenshotOnFailure(Scenario scenario) {
-        WebDriver driver = testContext.getDriver();
+        driver = DriverFactory.getDriver();
         if (scenario.isFailed() && driver != null) {
             try {
                 String timestamp = LocalDateTime.now()
@@ -62,18 +51,19 @@ public class Hooks {
         }
     }
 
-    @After(order = 1)
-    public void cleanupEmployee() {
-        ScenarioContext context = testContext.getScenarioContext();
-        boolean hasError = context.containsKey("errorMessage");
-        if (!hasError) {
-            new EmployeeCleanupHelper(testContext).cleanupTestEmployeeIfExists();
+    @After
+    public void after() {
+        try {
+            if (AddEmployeeSteps.employeeId != null && AddEmployeeSteps.isEmployeeCreated) {
+                String employeeId = AddEmployeeSteps.employeeId;
+                EmployeeListPO employeeListPage = PageGenerator.getSidebarPage(driver).openPIMPage();
+                employeeListPage.clickDeleteEmployeeButton(employeeId);
+                employeeListPage.clickConfirmDeleteButton();
+                Assert.assertTrue(employeeListPage.isEmployeeDeleted(employeeId));
+            }
+        } finally {
+            AddEmployeeSteps.employeeId = null;
+            DriverFactory.quitDriver();
         }
-        context.clearDataMap();
-    }
-
-    @After(order = 0)
-    public void quitDriver() {
-        testContext.quit();
     }
 }
