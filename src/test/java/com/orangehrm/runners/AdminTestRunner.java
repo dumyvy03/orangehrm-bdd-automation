de8420b125021c7f8c0d325967b71d1ddd21c33b
Du My Vy <dumyvy03@gmail.com>
2025-11-25 17:26:03 +0700

    Add AdminTestRunner class

---
+++ src/test/java/com/orangehrm/runners/AdminTestRunner.java
@@ -0,0 +1,20 @@
+package com.orangehrm.runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = {
+                "src/test/resources/features/admin/AddUser.feature",
+                "src/test/resources/features/admin/SearchUser.feature",
+                "src/test/resources/features/admin/DeleteUser.feature",
+        },
+        glue = {"com.orangehrm.steps",
+                "com.orangehrm.hooks",
+                "com.orangehrm.core"
+        },
+        plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
+        monochrome = true
+)
+public class AdminTestRunner extends AbstractTestNGCucumberTests {
+}
