6e061cdc4ef49ded58bf5fa33f9b2a10c878227b
Du My Vy <dumyvy03@gmail.com>
2025-11-25 23:26:22 +0700

    Add PimTestRunner class

---
+++ src/test/java/com/orangehrm/runners/PimTestRunner.java
@@ -0,0 +1,16 @@
+package com.orangehrm.runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = ("src/test/resources/features/pim"),
+        glue = {"com.orangehrm.steps",
+                "com.orangehrm.hooks",
+                "com.orangehrm.core"
+        },
+        plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
+        monochrome = true
+)
+public class PimTestRunner extends AbstractTestNGCucumberTests {
+}
