1d57b174efbd48ebed9469fb73ec40331d3cfed8
Du My Vy <dumyvy03@gmail.com>
2025-11-25 17:28:43 +0700

    Add LoginPUI class

---
+++ src/main/java/com/orangehrm/ui/login/LoginPUI.java
@@ -0,0 +1,16 @@
+package com.orangehrm.ui.login;
+
+public class LoginPUI {
+    public static final String USERNAME_TEXTBOX = "Name=username";
+
+    public static final String PASSWORD_TEXTBOX = "Name=password";
+
+    public static final String LOGIN_BUTTON = "Css=button.orangehrm-login-button";
+
+    public static final String USERNAME_ERROR_MESSAGE = "Xpath=//label[text()='Username']/parent::div//following-sibling::span";
+
+    public static final String PASSWORD_ERROR_MESSAGE = "Xpath=//label[text()='Password']/parent::div//following-sibling::span";
+
+    public static final String LOGIN_ERROR_MESSAGE = "Xpath=//div[@class='orangehrm-login-error']//p";
+}
+
