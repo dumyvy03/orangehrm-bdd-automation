86cc9894e87ba68094762d336288b4a8c5f62d72
Du My Vy <dumyvy03@gmail.com>
2025-11-26 13:32:40 +0700

    Move DashboardPUI to com.orangehrm.ui.dashboard package

--- src/main/java/com/orangehrm/ui/dashboard/DashboardPUI.java
+++ src/main/java/com/orangehrm/ui/dashboard/DashboardPUI.java
@@ -1,4 +1,4 @@
-package com.orangehrm.pages.pageuis.dashboard;
+package com.orangehrm.ui.dashboard;
 
 public class DashboardPUI {
     public static final String DASHBOARD_WIDGETS = "Css=div.orangehrm-dashboard-widget-body";
