8e9a1981d647c3d1e1a3180eac9e8e85844d05b9
Du My Vy <dumyvy03@gmail.com>
2025-11-26 13:20:21 +0700

    Update Cucumber feature path in LoginTestRunner

--- src/test/java/com/orangehrm/runners/LoginTestRunner.java
+++ src/test/java/com/orangehrm/runners/LoginTestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = ("src/test/resources/features/login/Login.feature"),
+        features = ("src/test/resources/features/login"),
         glue = {"com.orangehrm.steps",
                 "com.orangehrm.hooks",
                 "com.orangehrm.core"
