3725de783f293155a20ebdc64d71295222d139fa
Du My Vy <dumyvy03@gmail.com>
2025-11-26 23:34:21 +0700

    Add Allure Step annotations across AddEmployeePO methods

--- src/main/java/com/orangehrm/pages/pim/AddEmployeePO.java
+++ src/main/java/com/orangehrm/pages/pim/AddEmployeePO.java
@@ -1,8 +1,9 @@
-package com.orangehrm.pages.pageobjects.pim;
+package com.orangehrm.pages.pim;
 
-import com.orangehrm.commons.BasePage;
-import com.orangehrm.commons.PageGenerator;
-import com.orangehrm.pages.pageuis.pim.AddEmployeePUI;
+import com.orangehrm.core.BasePage;
+import com.orangehrm.core.PageGenerator;
+import com.orangehrm.ui.pim.AddEmployeePUI;
+import io.qameta.allure.Step;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 
@@ -17,30 +18,33 @@ public class AddEmployeePO extends BasePage {
         return getElementSize(driver, AddEmployeePUI.EMPLOYEE_AVATAR);
     }
 
+    @Step("Enter First Name: {0}")
     public void enterFirstNameTextbox(String firstName) {
         waitForElementVisible(driver, AddEmployeePUI.FIRSTNAME_TEXTBOX);
-        sleep(1);
         sendKeysElement(driver, AddEmployeePUI.FIRSTNAME_TEXTBOX, firstName);
+        sleep(1);
     }
 
+    @Step("Enter Last Name: {0}")
     public void enterLastNameTextbox(String lastName) {
         waitForElementVisible(driver, AddEmployeePUI.LASTNAME_TEXTBOX);
-        sleep(1);
         sendKeysElement(driver, AddEmployeePUI.LASTNAME_TEXTBOX, lastName);
-
-    }
-
-    public void uploadAvatar(String avatar) {
         sleep(1);
-        handleFileUpload(driver, avatar);
     }
 
-    public void enterEmployeeDetails(String firstName, String lastName, String avatar) {
+    @Step("Upload Avatar: {0}")
+    public void uploadAvatar(String avatar) {
+        handleFileUpload(driver, avatar);
+        sleep(1);
+    }
+
+    @Step("Enter employee details")
+    public void enterEmployeeDetails(String firstName, String lastName) {
         enterFirstNameTextbox(firstName);
         enterLastNameTextbox(lastName);
-        uploadAvatar(avatar);
     }
 
+    @Step("Click Save button")
     public PersonalDetailsPO clickSaveButton() {
         waitForElementClickable(driver, AddEmployeePUI.SAVE_BUTTON_ADD_EMPLOYEE);
         clickElement(driver, AddEmployeePUI.SAVE_BUTTON_ADD_EMPLOYEE);
@@ -48,18 +52,29 @@ public class AddEmployeePO extends BasePage {
         return PageGenerator.getPersonalDetailsPage(driver);
     }
 
+    @Step("Get first name error message")
+    public String getFirstNameErrorMessage() {
+        waitForElementVisible(driver, AddEmployeePUI.FIRSTNAME_ERROR_MESSAGE);
+        return getElementText(driver, AddEmployeePUI.FIRSTNAME_ERROR_MESSAGE);
+    }
+
+    @Step("Get last name error message")
     public String getLastNameErrorMessage() {
-        waitForElementVisible(driver, AddEmployeePUI.LASTNAME_ERROR_TEXT);
-        return getElementText(driver, AddEmployeePUI.LASTNAME_ERROR_TEXT);
+        waitForElementVisible(driver, AddEmployeePUI.LASTNAME_ERROR_MESSAGE);
+        return getElementText(driver, AddEmployeePUI.LASTNAME_ERROR_MESSAGE);
     }
 
+    @Step("Get avatar error message")
     public String getAvatarErrorMessage() {
-        waitForElementVisible(driver, AddEmployeePUI.AVATAR_ERROR_TEXTBOX);
-        return getElementText(driver, AddEmployeePUI.AVATAR_ERROR_TEXTBOX);
+        waitForElementVisible(driver, AddEmployeePUI.AVATAR_ERROR_MESSAGE);
+        sleep(1);
+        return getElementText(driver, AddEmployeePUI.AVATAR_ERROR_MESSAGE);
     }
 
+    @Step("Get generated employee ID value")
     public String getEmployeeIdValue() {
-        waitForElementPresence(driver, AddEmployeePUI.EMPLOYEE_ID_TEXTBOX);
+        waitForElementVisible(driver, AddEmployeePUI.EMPLOYEE_ID_TEXTBOX);
         return getAttributeValue(driver, AddEmployeePUI.EMPLOYEE_ID_TEXTBOX, "value");
     }
+
 }
