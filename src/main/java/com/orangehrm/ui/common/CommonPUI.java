bcf718942f2739c26643a62b3e49bb2610881bb3
Du My Vy <dumyvy03@gmail.com>
2025-11-28 00:03:05 +0700

    Move BasePUI to new package and rename to CommonPUI

--- src/main/java/com/orangehrm/ui/common/CommonPUI.java
+++ src/main/java/com/orangehrm/ui/common/CommonPUI.java
@@ -1,6 +1,7 @@
-package com.orangehrm.pages.pageuis.commons;
+package com.orangehrm.ui.common;
 
-public class BasePUI {
+public class CommonPUI {
     public static final String LOADING_ICON = "Css=div.oxd-loading-spinner";
+
     public static final String UPLOAD_FILE_TYPE = "Xpath=//input[@type='file']";
 }
