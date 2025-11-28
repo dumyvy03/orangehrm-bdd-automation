7a8079ad2f11b14dabfa54715f0a89d1317cc7ee
Du My Vy <dumyvy03@gmail.com>
2025-11-29 00:40:08 +0700

    Update imports and add Allure steps to EmployeeListPO

--- src/main/java/com/orangehrm/pages/pim/EmployeeListPO.java
+++ src/main/java/com/orangehrm/pages/pim/EmployeeListPO.java
@@ -1,8 +1,9 @@
-package com.orangehrm.pages.pageobjects.pim;
+package com.orangehrm.pages.pim;
 
-import com.orangehrm.commons.BasePage;
-import com.orangehrm.commons.PageGenerator;
-import com.orangehrm.pages.pageuis.pim.EmployeeListPUI;
+import com.orangehrm.core.BasePage;
+import com.orangehrm.core.PageGenerator;
+import com.orangehrm.ui.pim.EmployeeListPUI;
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 
 import java.util.List;
@@ -14,63 +15,72 @@ public class EmployeeListPO extends BasePage {
         this.driver = driver;
     }
 
+    @Step("Open Add Employee page")
     public AddEmployeePO openAddEmployeePage() {
         waitForElementClickable(driver, EmployeeListPUI.ADD_EMPLOYEE_LINK);
-        sleep(2);
         clickElement(driver, EmployeeListPUI.ADD_EMPLOYEE_LINK);
         waitForLoadingIconInvisible(driver);
         return PageGenerator.getAddEmployeePage(driver);
     }
 
+    @Step("Click Delete button")
     public void clickDeleteEmployeeButton(String employeeID) {
-        scrollToElement(driver, EmployeeListPUI.DELETE_BUTTON_BY_ID, employeeID);
+        waitForElementVisible(driver, EmployeeListPUI.DELETE_BUTTON_BY_ID, employeeID);
+        scrollIntoViewJS(driver, EmployeeListPUI.DELETE_BUTTON_BY_ID, employeeID);
         waitForElementClickable(driver, EmployeeListPUI.DELETE_BUTTON_BY_ID, employeeID);
-        sleep(1);
+        sleep(2);
         clickElement(driver, EmployeeListPUI.DELETE_BUTTON_BY_ID, employeeID);
     }
 
+    @Step("Click Delete button on the confirmation dialog")
     public void clickConfirmDeleteButton() {
         waitForElementClickable(driver, EmployeeListPUI.CONFIRM_DELETE_BUTTON);
-        sleep(1);
+        sleep(2);
         clickElement(driver, EmployeeListPUI.CONFIRM_DELETE_BUTTON);
     }
 
+    @Step("Verify employee with ID '{0}' is deleted")
     public boolean isEmployeeDeleted(String employeeID) {
         waitForLoadingIconInvisible(driver);
-        waitForElementInVisible(driver, EmployeeListPUI.EMPLOYEE_ID_IN_EMPLOYEE_TABLE, employeeID);
         return isElementUnDisplayed(driver, EmployeeListPUI.EMPLOYEE_ID_IN_EMPLOYEE_TABLE, employeeID);
     }
 
+    @Step("Click Search button")
     public void clickSearchButton() {
         waitForElementClickable(driver, EmployeeListPUI.SEARCH_BUTTON);
         sleep(1);
         clickElement(driver, EmployeeListPUI.SEARCH_BUTTON);
     }
 
-    public PersonalDetailsPO clickEditButtonEmployeeList() {
-        scrollToElement(driver, EmployeeListPUI.EDIT_BUTTON);
+    @Step("Click Edit button")
+    public PersonalDetailsPO clickEditButton() {
+        waitForElementVisible(driver, EmployeeListPUI.EDIT_BUTTON);
+        scrollIntoViewJS(driver, EmployeeListPUI.EDIT_BUTTON);
         waitForElementClickable(driver, EmployeeListPUI.EDIT_BUTTON);
-        sleep(1);
         clickElement(driver, EmployeeListPUI.EDIT_BUTTON);
         waitForLoadingIconInvisible(driver);
         return PageGenerator.getPersonalDetailsPage(driver);
     }
 
+    @Step("Enter Employee Id: {0}")
     public void enterEmployeeIdTextbox(String employeeId) {
         waitForElementVisible(driver, EmployeeListPUI.EMPLOYEE_ID_SEARCH_TEXTBOX);
-        sleep(1);
         sendKeysElement(driver, EmployeeListPUI.EMPLOYEE_ID_SEARCH_TEXTBOX, employeeId);
+        sleep(2);
     }
 
+    @Step("Enter Employee Name: {0}")
     public void enterEmployeeNameTextbox(String employeeName) {
+        sleep(2);
         waitForElementVisible(driver, EmployeeListPUI.EMPLOYEE_NAME_TEXTBOX);
-        sleep(1);
         sendKeysElement(driver, EmployeeListPUI.EMPLOYEE_NAME_TEXTBOX, employeeName);
+
     }
 
+    @Step("Verify employee '{0}' is displayed")
     public boolean isEmployeeDisplayed(String employeeName) {
         waitForElementVisible(driver, EmployeeListPUI.FIRSTNAME_IN_EMPLOYEE_TABLE);
-        scrollToElement(driver, EmployeeListPUI.FIRSTNAME_IN_EMPLOYEE_TABLE);
+        scrollIntoViewJS(driver, EmployeeListPUI.FIRSTNAME_IN_EMPLOYEE_TABLE);
         sleep(2);
         List<String> firstNames = getElementsText(driver, EmployeeListPUI.FIRSTNAME_IN_EMPLOYEE_TABLE);
         List<String> lastNames = getElementsText(driver, EmployeeListPUI.LASTNAME_IN_EMPLOYEE_TABLE);
@@ -84,5 +94,13 @@ public class EmployeeListPO extends BasePage {
             }
         }
         return false;
+    }
+
+    @Step("Get search result message")
+    public String getSearchResultMessage() {
+        waitForElementVisible(driver, EmployeeListPUI.SEARCH_RESULT_MESSAGE);
+        scrollIntoViewJS(driver, EmployeeListPUI.SEARCH_RESULT_MESSAGE);
+        sleep(2);
+        return getElementText(driver, EmployeeListPUI.SEARCH_RESULT_MESSAGE);
     }
 }
